acf478c2f39e5f7b25135a6e16d603115f1f807b
Hector Rodrigues <hectorrsa@outlook.com>
2023-01-21 00:00:53 -0300

    feat(core): add Socket.io

--- src/core/views/HomeView.vue
+++ src/core/views/HomeView.vue
@@ -3,5 +3,28 @@
 </template>
 
 <script>
-export default {};
+export default {
+  inject: ['$ws'],
+  methods: {
+    getAllGroupTasks() {
+      this.$ws.emit('group_tasks.getAll');
+
+      this.$ws.on('group_tasks.getAll', (res) => {
+        const groups = res.data;
+
+        if (groups.length) {
+          groups.forEach((group) => this.getTasksPerGroup(group.id));
+        }
+      });
+    },
+    getTasksPerGroup(group_id) {
+      this.$ws.emit('tasks.getPerGroup', group_id);
+
+      this.$ws.on('tasks.getPerGroup', (res) => console.log(res));
+    },
+  },
+  created() {
+    this.getAllGroupTasks();
+  },
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,17 @@ import App from './App.vue';
 import router from './core/config/router';
 import globalUri from '@/core/config/global_uri';
 import Axios from '@/core/plugins/axios';
+import { io } from 'socket.io-client';
+
+const socket = io(import.meta.env.VITE_BACKEND_URI_WS);
+
+socket.on('connect', () => {
+  console.log(socket.id);
+});
+
+socket.on('connect_error', (err) => {
+  console.log(`connect_error due to ${err.message}`);
+});
 
 import '@/core/plugins/bootstrap';
 
@@ -16,5 +27,6 @@ app.use(createPinia());
 app.use(router);
 
 app.provide('$axios', Axios);
+app.provide('$ws', socket);
 
 app.mount('#app');
